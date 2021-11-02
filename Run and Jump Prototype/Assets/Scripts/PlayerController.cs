b3c0004a538acaa4616a86d18c4820e96cf9cecf
Phoenix Schneider <10908806@uvu.edu>
2021-11-02 08:16:04 -0600

    working on particles

--- Run and Jump Prototype/Assets/Scripts/PlayerController.cs
+++ Run and Jump Prototype/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
-    private Animator playerAnim;
+   
+    
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +32,7 @@ public class PlayerController : MonoBehaviour
           playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
        
     }
@@ -36,6 +41,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
@@ -43,6 +49,8 @@ public class PlayerController : MonoBehaviour
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
         }
     }
 }
