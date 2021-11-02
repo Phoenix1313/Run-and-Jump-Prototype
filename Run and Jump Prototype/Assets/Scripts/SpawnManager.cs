4d3585afe161b9cfa070cf2f9724c86c15c3342d
Phoenix Schneider <10908806@uvu.edu>
2021-11-02 06:14:37 -0600

    Completed Make the world whiz bye

--- Run and Jump Prototype/Assets/Scripts/SpawnManager.cs
+++ Run and Jump Prototype/Assets/Scripts/SpawnManager.cs
@@ -8,11 +8,14 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript =
+            GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -20,8 +23,12 @@ public class SpawnManager : MonoBehaviour
     {
        
     }
-    void SpawnObstacle ()
+    void SpawnObstacle()
     {
-        Instantiate(obstaclePreFab, spawnPos, obstaclePreFab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePreFab, spawnPos, obstaclePreFab.transform.rotation);
+        }
+       
     }
 }
